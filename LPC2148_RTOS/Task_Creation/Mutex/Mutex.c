ad6a7ee921d2e7312ee92bb1c15fa59f39ec6637
Sunil-Kumar-1706 <v24ce1s6@gmail.com>
2025-07-31 19:25:58 +0530

    Add files via upload

---
+++ LPC2148_RTOS/Task_Creation/Mutex/Mutex.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include "FreeRTOS.h"
+#include "task.h"
+#include "Uart.h"
+#include "semphr.h"
+
+void task1(void *p);
+void task2(void *p);
+
+xSemaphoreHandle xMutex=NULL;
+
+int main()
+{  
+  
+  UART0_CONFIG();  
+
+  xMutex=xSemaphoreCreateMutex();  
+  if(xMutex!=NULL) {
+    xTaskCreate(task1,"task1",128,NULL,1,NULL);
+    xTaskCreate(task2,"task2",128,NULL,1,NULL);
+    vTaskStartScheduler();
+  }
+  while(1);
+}
+
+void task1(void *p)
+{
+  while(1) {
+    xSemaphoreTake(xMutex,portMAX_DELAY );
+    uart_string("Task1 Running\r\n");
+    xSemaphoreGive(xMutex );
+    vTaskDelay(50);
+  }
+}
+
+void task2(void *p)
+{
+  while(1)
+  {
+    xSemaphoreTake(xMutex,portMAX_DELAY );
+    uart_string("Task2 Running\r\n");
+    xSemaphoreGive(xMutex);
+    vTaskDelay(50);
+  }
+}
